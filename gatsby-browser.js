64995a02b5993c2c9c09d426050a5a1e28ba55b8
Vitor Buzinaro <vitor@close.io>
2022-07-12 12:50:02 -0300

    add "We're hiring" console.log (#87)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
 import './src/styles/global.scss';
 import './src/styles/prismjs/theme.css';
+
+console.log(
+  "\n%cHello! Welcome to The Making of Close!%c\n\n\n%cWe're hiring:%c https://www.close.com/careers\n",
+  'font-size: 24px; background-color: #1463ff; color: #fff; padding: 4px;',
+  '',
+  'font-size: 20px; background-color: #d61549; color: #fff; padding: 4px;',
+  'font-size: 18px;',
+);
